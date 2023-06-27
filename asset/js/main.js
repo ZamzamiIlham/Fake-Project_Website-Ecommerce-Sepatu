f5dd678038edf7eb405334f4949ce73bb2a49988
Zamzamillham <zamzamilham17@gmail.com>
2023-06-27 13:53:50 +0700

    first commit

---
+++ asset/js/main.js
@@ -0,0 +1,20 @@
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
+if (navToggle){
+    navToggle.addEventListener('click' , () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+if (navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+/*Membuat ketika menu di klik, akan menghilangkan show menu*/
+const navLink = document.querySelectorAll('.nav__link')
+const linkAction = () => {
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach ( n => n.addEventListener('click', linkAction))
